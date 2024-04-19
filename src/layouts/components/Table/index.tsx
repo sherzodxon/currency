37660aa544b537bdbb6d170bafb5453e71db9331
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2024-04-19 17:05:33 +0500

    responseive is end

--- src/layouts/components/Table/index.tsx
+++ src/layouts/components/Table/index.tsx
@@ -1,9 +1,18 @@
-import React, { useRef } from 'react';
-import {Button, Form, Input, InputRef, Result, Skeleton, Table,Tag} from "antd";
-import { FiSearch } from "react-icons/fi";
+import React, {useRef} from 'react';
+import {
+    Button,
+    Form,
+    Input,
+    InputRef,
+    Result,
+    Skeleton,
+    Table,
+    Tag
+} from "antd";
+import {FiSearch} from "react-icons/fi";
 import {ColumnsType} from 'antd/es/table';
 import {useEffect, useState} from "react";
-import {ArrowUpOutlined,ArrowDownOutlined} from '@ant-design/icons';
+import {ArrowUpOutlined, ArrowDownOutlined} from '@ant-design/icons';
 import './index.scss'
 
 interface Currency {
@@ -16,34 +25,38 @@ interface Currency {
 }
 
 const TableSection : React.FC = () => {
-    const [data,setData] = useState < Currency[] > ([]);
-    const [currencies,setCurrencies] = useState < Currency[] > ([]);
-    const [isLoading,setLoading] = useState (true);
-    const [searchData,setSearchData]=useState<Currency[]>([]);
-    const [searchValue,setSearchValue]=useState<String>("")
-    const [error,setError] = useState < Boolean > (false);
-    const searchRef = useRef<InputRef>(null);
-    const url:any = process.env.REACT_APP_BASE_URL;
+    const [data,
+        setData] = useState < Currency[] > ([]);
+    const [currencies,
+        setCurrencies] = useState < Currency[] > ([]);
+    const [isLoading,
+        setLoading] = useState(true);
+    const [searchData,
+        setSearchData] = useState < Currency[] > ([]);
+    const [searchValue,
+        setSearchValue] = useState < String > ("")
+    const [error,
+        setError] = useState < Boolean > (false);
+    const searchRef = useRef < InputRef > (null);
+    const url : any = process.env.REACT_APP_BASE_URL;
     type FieldType = {
         code?: string;
         date?: string;
-      };
-    function diffColorChanger(params:number) {
-        if (params>0) {
+    };
+    function diffColorChanger(params : number) {
+        if (params > 0) {
             return "red"
-            
-        }
-        else if (params === 0) {
+
+        } else if (params === 0) {
             return "cyan"
-        }
-        else
-        return "green"
+        } else 
+            return "green"
     }
-    function diffIconChanger(params:number) {
-        if (params>0) {
+    function diffIconChanger(params : number) {
+        if (params > 0) {
             return false
-        }
-        else return true
+        } else 
+            return true
     }
     useEffect(() => {
         const fetchCurrencies = async() => {
@@ -65,37 +78,38 @@ const TableSection : React.FC = () => {
                 setCurrencies(mappedCurrencies);
                 setLoading(false)
             } catch (error) {
-               setError(true)
+                setError(true)
             }
         };
         fetchCurrencies();
     }, []);
-    useEffect(()=>{
-       if (searchData.length) {
-        setData(searchData)
-       }
-       else setData(currencies)
-    },[currencies,searchValue])
+    useEffect(() => {
+        if (searchData.length) {
+            setData(searchData)
+        } else 
+            setData(currencies)
+    }, [currencies, searchValue])
     function handleSearchInput() {
         if (searchRef.current) {
-            const searchValue:any = searchRef.current.input?.value
+            const searchValue : any = searchRef.current.input
+                ?.value
             setSearchValue(searchValue)
             const foundData = currencies.filter((el) => {
-                
+
                 const searchRegExp = new RegExp(searchValue, "gi");
                 const searchText = `${el.CcyNm_UZ}`;
                 return searchText.match(searchRegExp);
-    
-            })  
+
+            })
             setSearchData(foundData)
-           }
+        }
     }
     const columns : ColumnsType < Currency > = [
         {
             title: 'Nomi',
             dataIndex: 'CcyNm_UZ',
             key: 'name',
-            sorter: (a, b) => a.CcyNm_UZ.length - b.CcyNm_UZ.length,
+            sorter: (a, b) => a.CcyNm_UZ.length - b.CcyNm_UZ.length
         }, {
             title: 'Kodi',
             dataIndex: 'Ccy',
@@ -104,13 +118,18 @@ const TableSection : React.FC = () => {
             title: "Kursi",
             dataIndex: 'Rate',
             key: 'rate',
-            sorter: (a, b) => a.Rate - b.Rate,
+            sorter: (a, b) => a.Rate - b.Rate
         }, {
             title: "O'zgarishi",
             dataIndex: 'Diff',
             key: 'diff',
-            render:(diff)=>(
-              <Tag bordered={false} icon={diffIconChanger(-diff)?<ArrowUpOutlined/>:<ArrowDownOutlined/>} color={diffColorChanger(-diff)}>{diff}</Tag>
+            render: (diff) => (
+                <Tag
+                    bordered={false}
+                    icon={diffIconChanger(-diff)
+                    ? <ArrowUpOutlined/>
+                    : <ArrowDownOutlined/>}
+                    color={diffColorChanger(-diff)}>{diff}</Tag>
             )
         }, {
             title: "Sana",
@@ -118,43 +137,99 @@ const TableSection : React.FC = () => {
             key: 'date'
         }
     ];
+    const onFinish = async(values : any) => {
+        try {
+            const response = values.code
+                ? await fetch(`${url}${values.code}/${values.date}/`)
+                : await fetch(`${url}all/${values.date}/`);
+            if (!response.ok) {
+                setError(true)
+            }
+            setLoading(true)
+            const data = await response.json();
+            const historyCurrency = data.map((currency : any) => ({
+                key: currency.id,
+                CcyNm_UZ: currency.CcyNm_UZ,
+                Rate: currency.Rate,
+                Date: currency.Date,
+                Ccy: currency.Ccy,
+                Diff: currency.Diff
+
+            }))as Currency[];
+            setCurrencies(historyCurrency);
+            setLoading(false)
+        } catch (error) {
+            setError(true)
+        }
+    };
     if (error) {
-        return(
-            <Result
+        return(<Result 
             status="404"
-            title="403"
+            title="403" 
             subTitle="Iltimos! Qaytadan urinib ko'ring."
-            extra={<Button onClick={()=>window.location.reload()} type="primary">Qayta boshlash</Button>}
-             />
-        )
-    }
+            extra={<Button 
+            onClick={()=>window.location.reload()} 
+            type="primary">Qayta boshlash</Button>}/>     
+        ) } 
     else
     return (
         <div>
             <Skeleton loading={isLoading} active>
-                <div className="table-head">   
-                <Input type='text' onChange={handleSearchInput} ref={searchRef} style={{width:"30rem"}} size="large" color='red' placeholder="Qidirish" prefix={<FiSearch color='gray' style={{width:"2rem", height:"2rem"}} />} />
-                <Form className="table-history-wrapper">
-                    <Form.Item<FieldType>  name="code"  rules={[{ required: true, message: 'Iltimos kodni kiriting!' }]}>
-                    <Input size="large" style={{textTransform:'uppercase',width:"60px"}} type='text' placeholder='USD' maxLength={3} pattern="[A-Z]{3}" />
-                    </Form.Item>
-                    <Form.Item<FieldType>  name="date"  rules={[{ required: true, message: 'Iltimos vaqtni kiriting!' }]}>
-                    <Input size='large' type='date' style={{maxWidth:"15rem"}}/>
-                    </Form.Item>
-                    
-                    <Form.Item>
-                    <Button size='large' type="primary" htmlType="submit">Yuborish
-                   </Button>
-    </Form.Item>
-                </Form>
+                <div className="table-head">
+                    <Input
+                        type='text'
+                        onChange={handleSearchInput}
+                        ref={searchRef}
+                        style={{
+                        width: "30rem"
+                    }}
+                        className='table-search-input'
+                        size="large"
+                        color='red'
+                        placeholder="Qidirish"
+                        prefix={< FiSearch color = 'gray' style = {{width:"2rem", height:"2rem"}}/>}/>
+                    <Form onFinish={onFinish} className="table-history-wrapper">
+                        <Form.Item<FieldType>
+                            name="code">
+                            <Input
+                                className='table-form-code'
+                                size="large"
+                                style={{
+                                textTransform: 'uppercase',
+                            }}
+                                type='text'
+                                placeholder='USD'
+                                maxLength={3}/>
+                        </Form.Item>
+                        <Form.Item<FieldType>
+                            name="date" rules={[{
+                                    required: true,
+                                    message: 'Iltimos vaqtni kiriting!'
+                                }
+                            ]}>
+                            <Input
+                                className='table-form-date'
+                                size='large'
+                                type='date'
+                               />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                size='large'
+                                className='table-form-button'
+                                type="primary"
+                                htmlType="submit">Yuborish
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </div>
-            <Table<Currency>
-                dataSource={data}
-                columns={columns}
-                />
-            </Skeleton>
-           
-        </div>
+                <Table<Currency>
+                    dataSource={data}
+                    columns={columns}
+                    />
+                </Skeleton>
+
+            </div>
     )
 }
 export default TableSection
